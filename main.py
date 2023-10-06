b98c00b4854a7ee99d78256e9d651e9a5acc0399
Robert Horvat <robert.horvath93@gmail.com>
2023-10-06 23:38:29 +0300

    Extracting info of interest

--- main.py
+++ main.py
@@ -6,13 +6,20 @@ URL = "http://programmer100.pythonanywhere.com/tours/"
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
-def scrape(url, headers=HEADERS):
+def scrape(url):
     # Scrape the page source
-    response = requests.get(URL)
+    response = requests.get(URL, headers=HEADERS)
     source = response.text
     return source
 
 
+def extract(source):
+    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+    value = extractor.extract(source)["tours"]
+    return value
+
 if __name__ == "__main__":
-    print(scrape(URL))
+    scraped = scrape(URL)
+    extracted = extract(scraped)
+    print(extracted)
 
