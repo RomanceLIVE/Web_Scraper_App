b04dd01d2018641bb4f22d5444b64369b1b39a28
Robert Horvat <robert.horvath93@gmail.com>
2023-10-07 22:07:45 +0300

    Extracting info of interest

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import requests
 import selectorlib
 
-
 URL = "http://programmer100.pythonanywhere.com/tours/"
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
 
 def scrape(url):
     # Scrape the page source
@@ -18,8 +18,33 @@ def extract(source):
     value = extractor.extract(source)["tours"]
     return value
 
+
+def send_email():
+    print("Email sent")
+
+
+def store(extracted):
+    if extracted != "No upcoming tours":
+        # Check for duplicates
+        with open("data.txt", "r") as file:
+            existing_data = file.read().splitlines()
+        if extracted not in existing_data:
+            with open("data.txt", "a") as file:  # a for append
+                file.write(extracted + "\n")
+
+
+def read(extracted):
+    with open("data.txt", "r") as file:
+        return file.read()
+
+
 if __name__ == "__main__":
     scraped = scrape(URL)
     extracted = extract(scraped)
     print(extracted)
-
+    store(extracted)
+    content = read(extracted)
+    if extracted != "No upcoming tours":
+        if extracted not in content:
+            store(extracted)  # only store when event new
+            send_email()
